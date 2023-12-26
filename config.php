29687f586f9a8ee2266b846444aca39a27ee4512
Crisciany Silva <crisciany.souza@librecode.coop>
2023-12-26 09:46:00 -0400

    Changes name of variable to develop
    Signed-off-by: Crisciany Silva <crisciany.souza@librecode.coop>

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'production' => false,
+    'production' => getenv('APP_ENV') === 'production',
     'baseUrl' => '',
     'title' => 'LibreSign - Electronic signature of digital documents',
     'description' => 'Electronic signature of digital documents',
