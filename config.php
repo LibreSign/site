acb88f04e385d7d79f070464e319da72ef58abad
Vitor Mattos <vitor@php.rio>
2023-12-27 19:28:52 -0300

    Collection to redirect
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- config.php
+++ config.php
@@ -5,5 +5,22 @@ return [
     'baseUrl' => '',
     'title' => 'LibreSign - Electronic signature of digital documents',
     'description' => 'Electronic signature of digital documents',
-    'collections' => [],
+    'collections' => [
+        'redirect' => [
+            'extends' => '_layouts.redirect301',
+            'path' => function($page) {
+                return $page->from;
+            },
+            'items' => [
+                [
+                    'from' => '/teste',
+                    'to' => '/',
+                ],
+                [
+                    'from' => '/teste2',
+                    'to' => '/',
+                ],
+            ],
+        ]
+    ],
 ];
