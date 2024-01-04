4b3750f36fb4614a4402a6e16fc9af754a97f613
Crisciany Silva <crisciany.souza@librecode.coop>
2024-01-04 16:29:46 -0400

    More chenges to implements blog
    Signed-off-by: Crisciany Silva <crisciany.souza@librecode.coop>

---
+++ source/_layouts/team-member.blade.php
@@ -0,0 +1,70 @@
+@extends('_layouts.main')
+
+@section('body')
+    <!-- ====== Banner Start ====== -->
+    <section class="ud-page-banner">
+        <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+            <div class="ud-banner-content">
+                <h1>Author Page</h1>
+            </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    <!-- ====== Banner End ====== -->
+
+    
+    <section id="team" class="ud-team">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="ud-section-title mx-auto text-center">
+                <h2>Teste</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Praesent faucibus, elit in suscipit dictum, dui est egestas 
+                    odio, a viverra nulla dui non urna. Fusce sed quam rhoncus, 
+                    maximus nunc quis, convallis urna. Maecenas ut varius dolor. 
+                    Sed placerat ex at quam bibendum, tempus luctus sem malesuada. 
+                    In vestibulum semper risus, sed faucibus ex pretium vitae. 
+                    Etiam sollicitudin ex nec aliquam pulvinar. Integer 
+                    ullamcorper vel dolor id fringilla.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="ud-single-team wow fadeInUp" data-wow-delay=".1s">
+                <div class="ud-team-image-wrapper mb-5">  
+                  <img
+                    src="#"
+                    alt="autor_img"
+                    class="shape shape-1 mb-5"
+                  />
+                </div>
+                <div class="ud-team-info">
+                  <h5>Adveen Desuza</h5>
+                  <h6>UI Designer</h6>
+                </div>
+                <ul class="ud-team-socials">
+                  <li>
+                    <a href="https://twitter.com/MusharofChy">
+                      <i class="lni lni-github-original"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://twitter.com/MusharofChy">
+                      <i class="lni lni-linkedin-original"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>  
+@endsection
