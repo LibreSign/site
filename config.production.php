05e97184c8c7c0b0cdd80733e4d45f97a42805d2
Vitor Mattos <vitor@php.rio>
2024-03-13 16:16:10 -0300

    Change file name
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- config.production.php
+++ config.production.php
@@ -4,5 +4,5 @@ return [
     'production' => true,
     'matomo_container' => '8jNjdh8C',
     'form_url' => 'https://contactform.libresign.coop',
-    'url_captcha' => 'https://contactform.libresign.coop/reload_captcha.php',
+    'url_captcha' => 'https://contactform.libresign.coop/captcha.php',
 ];
