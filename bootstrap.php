c2231a2f4c1c59124daed66a6924da8298622d60
Vitor Mattos <vitor@php.rio>
2024-04-03 23:29:59 -0300

    feat: Move listener to class
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,8 @@
 <?php
 
+use App\Listeners\RemoveDeletedTranslations;
 use TightenCo\Jigsaw\Jigsaw;
 use ElaborateCode\JigsawLocalization\LoadLocalization;
-
-$events->beforeBuild([LoadLocalization::class]);
 
 /** @var \Illuminate\Container\Container $container */
 /** @var \TightenCo\Jigsaw\Events\EventBus $events */
@@ -19,21 +18,7 @@ $events->beforeBuild([LoadLocalization::class]);
  * });
  */
 
-$events->afterBuild(function(Jigsaw $jigsaw) {
-    $toTranslate = file_get_contents('lang/to_translate.json');
-    $toTranslate = json_decode($toTranslate, true);
-    foreach(glob('lang/*') as $path) {
-        $langFile = $path . '/main.json';
-        if (is_dir($path) && file_exists($langFile)) {
-            $translated = file_get_contents($langFile);
-            $translated = json_decode($translated, true);
-            foreach (array_keys($translated) as $text) {
-                if (!array_key_exists($text, $toTranslate)) {
-                    unset($translated[$text]);
-                }
-            }
-            file_put_contents($langFile, json_encode($translated, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT) . PHP_EOL);
-        }
-    }
-    unlink('lang/to_translate.json');
-});
+$events->beforeBuild([
+    LoadLocalization::class,
+]);
+$events->afterBuild([RemoveDeletedTranslations::class]);
---
+++ listeners/RemoveDeletedTranslations.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Listeners;
+
+use TightenCo\Jigsaw\Jigsaw;
+
+class RemoveDeletedTranslations
+{
+    public function handle(Jigsaw $jigsaw)
+    {
+        $toTranslate = file_get_contents('lang/to_translate.json');
+        $toTranslate = json_decode($toTranslate, true);
+        foreach(glob('lang/*') as $path) {
+            $langFile = $path . '/main.json';
+            if (is_dir($path) && file_exists($langFile)) {
+                $translated = file_get_contents($langFile);
+                $translated = json_decode($translated, true);
+                foreach (array_keys($translated) as $text) {
+                    if (!array_key_exists($text, $toTranslate)) {
+                        unset($translated[$text]);
+                    }
+                }
+                file_put_contents($langFile, json_encode($translated, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT) . PHP_EOL);
+            }
+        }
+        unlink('lang/to_translate.json');
+    }
+}
