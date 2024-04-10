cc465accc976267a4d1510e0022168d0b37e32a3
Crisciany Silva <crisciany.souza@librecode.coop>
2024-04-10 16:34:49 -0400

    remove current_path_locale($page)
    Signed-off-by: Crisciany Silva <crisciany.souza@librecode.coop>

--- source/_layouts/footer.blade.php
+++ source/_layouts/footer.blade.php
@@ -91,7 +91,7 @@
                 <div class="col-md-8">
                     <ul class="ud-footer-bottom-left">
                         <li>
-                            <a href="{{ $page->baseUrl }}privacy-policy">{{ $page->t("Privacy policy",current_path_locale($page))}}</a>
+                            <a href="{{ $page->baseUrl }}privacy-policy">{{ $page->t("Privacy policy")}}</a>
                         </li>
                     </ul>
                 </div>
