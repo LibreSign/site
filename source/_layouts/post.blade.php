fb28ea81b20727c5dbe23bed5b6031a4855c4082
Vitor Mattos <vitor@php.rio>
2024-04-22 12:01:34 -0300

    feat: condition to hide articles and banner
    When isn't a post, don't need to display banner and articles
    
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -19,31 +19,33 @@
     <section class="ud-blog-details">
       <div class="container">
         <div class="row">
-          <div class="col-lg-12">
-            <div class="ud-blog-details-image">
-              <img
-                src="{{ $page->banner }}"
-                alt="blog details"
-              />
-              <div class="ud-blog-overlay">
-                <div class="ud-blog-overlay-content">
-                  <div class="ud-blog-author">
-                    <img src="https://www.gravatar.com/avatar/{{$page->gravatar}}?size=40"
-                    alt="{{ $page->author }}" />
-                    <span>
-                      By <a href="{{ $page->baseUrl }}team/{{ \Illuminate\Support\Str::slug($page->author) }}"> {{ $page->author }} </a>
-                    </span>
-                  </div>
+          @if ($page->banner)
+            <div class="col-lg-12">
+              <div class="ud-blog-details-image">
+                <img
+                  src="{{ $page->banner }}"
+                  alt="blog details"
+                />
+                <div class="ud-blog-overlay">
+                  <div class="ud-blog-overlay-content">
+                    <div class="ud-blog-author">
+                      <img src="https://www.gravatar.com/avatar/{{$page->gravatar}}?size=40"
+                      alt="{{ $page->author }}" />
+                      <span>
+                        By <a href="{{ $page->baseUrl }}team/{{ \Illuminate\Support\Str::slug($page->author) }}"> {{ $page->author }} </a>
+                      </span>
+                    </div>
 
-                  <div class="ud-blog-meta">
-                    <p class="date">
-                      <i class="lni lni-calendar"></i> <span>{{ date('F j, Y', $page->date) }}</span>
-                    </p>
+                    <div class="ud-blog-meta">
+                      <p class="date">
+                        <i class="lni lni-calendar"></i> <span>{{ date('F j, Y', $page->date) }}</span>
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          @endif
 
           <div class="col-lg-8">
             <div class="ud-blog-details-content text-justify">
@@ -53,42 +55,42 @@
             </div>
           </div>
 
-          <div class="col-lg-4">
-            <div class="ud-blog-sidebar">
-              <div class="ud-articles-box">
-                <h3 class="ud-articles-box-title">Last Articles</h3>
-                <ul class="ud-articles-list">
-                  @php $count = 0; @endphp
-                  @foreach($posts as $article)
-                    @php $count++; @endphp
-                  
-                    @if($count >= 4) 
-                      @break 
-                    @endif
-                    
-                    <li>
-                      <div class="ud-article-image">
-                        <a href="{{ $page->baseUrl }}team/{{ \Illuminate\Support\Str::slug($page->author) }}">
-                          <img
-                            src="https://www.gravatar.com/avatar/{{ $article->gravatar }}"
-                            alt="{{ $article->author }}"
-                          />
-                        </a>
-                      </div>
-                      <div class="ud-article-content">
-                        <h5 class="ud-article-title">
-                          <a href="{{ $article->getUrl() }}">
-                            {{ $article->title }}
+          @if ($page->author)
+            <div class="col-lg-4">
+              <div class="ud-blog-sidebar">
+                <div class="ud-articles-box">
+                  <h3 class="ud-articles-box-title">Last Articles</h3>
+                  <ul class="ud-articles-list">
+                    @php $count = 0; @endphp
+                    @foreach($posts as $article)
+                      @php $count++; @endphp
+                      @if($count >= 4)
+                        @break
+                      @endif
+                      <li>
+                        <div class="ud-article-image">
+                          <a href="{{ $page->baseUrl }}team/{{ \Illuminate\Support\Str::slug($page->author) }}">
+                            <img
+                              src="https://www.gravatar.com/avatar/{{ $article->gravatar }}"
+                              alt="{{ $article->author }}"
+                            />
                           </a>
-                        </h5>
-                        <a href="{{ $page->baseUrl }}team/{{ \Illuminate\Support\Str::slug($page->author) }}"> {{ $page->author }} </a>
-                      </div>
-                    </li>
-                  @endforeach
-                </ul>
+                        </div>
+                        <div class="ud-article-content">
+                          <h5 class="ud-article-title">
+                            <a href="{{ $article->getUrl() }}">
+                              {{ $article->title }}
+                            </a>
+                          </h5>
+                          <a href="{{ $page->baseUrl }}team/{{ \Illuminate\Support\Str::slug($page->author) }}"> {{ $page->author }} </a>
+                        </div>
+                      </li>
+                    @endforeach
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
+          @endif
         </div>
       </div>
     </section>
