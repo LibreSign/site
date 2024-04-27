e024ed4f92ec13240a81702151a53c4d9133ed8c
Vitor Mattos <vitor@php.rio>
2024-04-27 10:23:40 -0300

    chore: look files and folders to search by temporary files
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- listeners/RemoveTranslationFiles.php
+++ listeners/RemoveTranslationFiles.php
@@ -17,9 +17,19 @@ class RemoveTranslationFiles
         $this->filesystem = $jigsaw->getFilesystem();
         $this->jigsaw = $jigsaw;
         $this->langs = $this->jigsaw->getSiteData()->localization->keys()->all();
-        $source = $this->jigsaw->getSourcePath();
-        $path = "{$source}/_posts";
-        $items = collect($this->filesystem->files($path))
+        $path = $this->jigsaw->getSourcePath();
+        collect($this->filesystem->directories($path))
+            ->filter(function ($folder) {
+                foreach ($this->langs as $lang) {
+                    if ($folder->getFilename() === $lang) {
+                        return true;
+                    }
+                }
+                return false;
+            })->each(function ($folder) {
+                $this->filesystem->deleteDirectory($folder->getPathName(), false);
+            });
+        collect($this->filesystem->files($path))
             ->filter(function ($file) {
                 foreach ($this->langs as $lang) {
                     if (Str::startsWith($file->getFilename(), $lang . '_')) {
