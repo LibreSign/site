14da408ecf0b0dda68355ae610b6542051992956
Vitor Mattos <vitor@php.rio>
2024-04-27 13:14:46 -0300

    feat: make possible translate pages
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- listeners/PrepareTranslationFiles.php
+++ listeners/PrepareTranslationFiles.php
@@ -28,6 +28,36 @@ class PrepareTranslationFiles
         );
         $this->frontMatterParser = new FrontMatterParser($parser);
         $this->handleCollections();
+        $this->handlePages();
+    }
+
+    private function handlePages(): void
+    {
+        $path = $this->jigsaw->getSourcePath();
+        $self = $this;
+        collect($this->filesystem->files($path))
+            ->filter(function ($file) {
+                if (str_starts_with($file->getFilename(), '_')) {
+                    return false;
+                }
+                if (str_starts_with($file->getRelativePathname(), '_')) {
+                    return false;
+                }
+                if (!in_array($file->getExtension(), ['markdown', 'md', 'mdown'])
+                    && !Str::contains($file->getFilename(), '.blade.')
+                ) {
+                    return false;
+                }
+                foreach ($this->langs as $lang) {
+                    if (Str::startsWith($file->getFilename(), $lang . '_')) {
+                        return false;
+                    }
+                }
+                return true;
+            })
+            ->each(function ($file) use ($path, $self) {
+                $self->copyOriginalFilesToTranslatePages($file, $path);
+            });
     }
 
     private function handleCollections(): void
@@ -59,23 +89,43 @@ class PrepareTranslationFiles
     public function copyOriginalFilesToTranslatePages(SplFileInfo $file) {
         $content = file_get_contents($file->getPathName());
         $post = $this->frontMatterParser->getFrontMatter($content);
-        $body = $this->frontMatterParser->getContent($content);
-        $page = $this->jigsaw->getCollection('page');
+        $isCollection = $this->isInCollectionDirectory($file);
         $this->items[] = $file;
         foreach ($this->langs as $lang) {
-            $page->updateTranslation($lang, $post['title']);
-            $page->updateTranslation($lang, $post['description']);
-            $page->updateTranslation($lang, $body);
-            $translatedName = __($page, $post['title'], $lang);
-            // Don't create translated page if haven't translated title
-            if ($translatedName === $post['title']) {
+            if ($lang === packageDefaultLocale()) {
                 continue;
             }
-            $translatedName = $lang . '_' . Str::slug($translatedName);
+            $translatedName = $file->getFilename();
+            if ($post) {
+                if (isset($post['title'])) {
+                    $translatedTitle = __($this->jigsaw->getSiteData(), $post['title'], $lang);
+                    // Don't create translated page if haven't translated title
+                    if ($translatedTitle === $post['title']) {
+                        continue;
+                    }
+                    if (!$isCollection) {
+                        $translatedName = Str::slug($translatedName);
+                        $translatedName = $this->translateFilename($file, $translatedName);
+                    }
+                }
+            }
+            if ($isCollection) {
+                $translatedName = $lang . '_' . $translatedName;
+            } else {
+                $translatedName = $lang . '/' . $translatedName;
+                $path = str_replace(
+                    $file->getFilename(),
+                    '',
+                    $file->getPathName()
+                );
+                if (!is_dir($path . $lang)) {
+                    mkdir($path . $lang);
+                }
+            }
             // Create temporary file to be possible create the path of this file
             $destination = str_replace(
                 $file->getFilename(),
-                $this->translateFilename($file, $translatedName),
+                $translatedName,
                 $file->getPathName()
             );
             $this->filesystem->copy(
@@ -88,11 +138,38 @@ class PrepareTranslationFiles
 
     private function translateFilename(SplFileInfo $file, $translated): string
     {
-        if (in_array($file->getExtension(), ['markdown', 'md', 'mdown'])) {
-            return $translated . $file->getExtension();
-        }
-        $exploded = explode('.blade.', $file->getFilename());
+        $exploded = explode('.', $file->getFilename());
         $exploded[0] = $translated;
-        return implode('.blade.', $exploded);
+        return implode('.', $exploded);
+    }
+
+    private function isInCollectionDirectory($file)
+    {
+        $relativePath = str_replace(
+            $this->jigsaw->getSourcePath() . '/',
+            '',
+            $file->getPathName()
+        );
+
+        return Str::startsWith($relativePath, '_') && $this->hasCollectionNamed($this->getCollectionName($file));
+    }
+
+    private function hasCollectionNamed($candidate)
+    {
+        if ($this->jigsaw->getSiteData()->get('collections')?->get($candidate)) {
+            return true;
+        }
+        return false;
+    }
+
+    private function getCollectionName($file)
+    {
+        $relativePath = str_replace(
+            $this->jigsaw->getSourcePath() . '/',
+            '',
+            $file->getPathName()
+        );
+        $relativePath = explode('/', $relativePath);
+        return substr($relativePath[0], 1);
     }
 }
