12eeed691d03de87a67382e614cab64ede959cb2
Vitor Mattos <vitor@php.rio>
2024-06-04 13:29:02 -0300

    fix: remove webpack from build
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,7 +8,6 @@ mix.jigsaw()
     .js('source/_assets/js/main.js', 'js')
     .css('source/_assets/css/main.css', 'css', [
         require('postcss-import'),
-        require('tailwindcss'),
     ])
     .sass('source/_assets/scss/ud-styles.scss', 'css')
     .sass('source/_assets/css/main.scss', 'css')
