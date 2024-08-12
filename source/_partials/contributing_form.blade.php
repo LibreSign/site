eaaced785fcc3e2cffebd44afb2b54b9cd4e46da
Crisciany <criscianysilva1997@gmail.com>
2024-08-12 10:36:59 -0400

    adjustement div values to contributing
    Signed-off-by: Crisciany <criscianysilva1997@gmail.com>

--- source/_partials/contributing_form.blade.php
+++ source/_partials/contributing_form.blade.php
@@ -32,94 +32,92 @@
         <div class = "col-12 col-lg-4 p-5 text-center"> 
             
             <div class="row">
-                <div class="col-12" style="display:none">
-                    <h4 class="me-2 card-title mb-4"><i class="lni lni-protection"></i> {{ $page->t('Safe Donate') }}</h4>
-                    <nav>                    
-                        <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                            <button class="text-dark-emphasis nav-link ud-feature-title ud-feature-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">{{$page->t('Donate one time')}}</button>
-                            <button class="text-dark-emphasis nav-link ud-feature-title ud-feature-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">{{$page->t('Monthly')}}</button>
-                            
-                        </div>
-                    </nav>
-                    <div class="tab-content " id="nav-tabContent">
-                        <div class="tab-pane fade show active mt-5" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-                            @php 
-                            $countryValues = $page->getCountry(current_path_locale($page));
-                            @endphp
-                            <div class="row">
-                            @if(!empty($countryValues["value"]))  
-                                @foreach($countryValues["value"] as $value)
-                                <div class="col-12 col-sm-6 col-lg-4 mb-2"  >
-                                    <a class="ud-main-btn" style=" padding: 20px ;width:100" id="donateValue" onclick="getValue({{ $value }})">{{$countryValues["symbol"]." ".$value }}</a>
-                                </div>
-                                @endforeach
-                            @endif    
-                            </div>                 
-        
-                            <div class="input-group mb-5 mt-5">
-                            <input type="text" class="form-control" aria-label="Text input with dropdown button" id="showValue">
-                            <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">{{ current_path_locale($page) }}</button>
-                            
-                            <ul class="dropdown-menu dropdown-menu-end">
-                                @foreach($page->donateCoin as $iten)
-                                @foreach($iten as $coin)
-                                    <li><a class="dropdown-item">{{$coin}}</a></li>                  
-                                @endforeach
-                                @endforeach
-                            </ul>
+                <form>
+                    <div class="col-12 tab">
+                        <h4 class="me-2 card-title mb-4"><i class="lni lni-protection"></i> {{ $page->t('Safe Donate') }}</h4>
+                        <nav>                    
+                            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                                <button class="text-dark-emphasis nav-link ud-feature-title ud-feature-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">{{$page->t('Donate one time')}}</button>
+                                <button class="text-dark-emphasis nav-link ud-feature-title ud-feature-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">{{$page->t('Monthly')}}</button>
+                                
                             </div>
-        
-                            <div class="row">
-                            <div class="col-12">
-                                <button type="submit" class="ud-main-btn">{{ $page->t('Donate one time') }}</button>
-                            </div>
-                            </div>
-                            
-                        </div>
-                        <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                        </nav>
+                        <div class="tab-content " id="nav-tabContent">
                             <div class="tab-pane fade show active mt-5" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
                                 @php 
                                     $countryValues = $page->getCountry(current_path_locale($page));
                                 @endphp
                                 <div class="row">
                                     @if(!empty($countryValues["value"]))  
-                                    @foreach($countryValues["value"] as $value)
+                                        @foreach($countryValues["value"] as $value)
                                         <div class="col-12 col-sm-6 col-lg-4 mb-2"  >
                                             <a class="ud-main-btn" style=" padding: 20px ;width:100" id="donateValue" onclick="getValue({{ $value }})">{{$countryValues["symbol"]." ".$value }}</a>
                                         </div>
-                                    @endforeach
-                                    @endif
-                                </div>
-                
+                                        @endforeach
+                                    @endif    
+                                </div>                 
+            
                                 <div class="input-group mb-5 mt-5">
-                                    <input type="text" class="form-control" aria-label="Text input with dropdown button">
+                                    <input type="text" class="form-control" aria-label="Text input with dropdown button" id="showValue">
                                     <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">{{ current_path_locale($page) }}</button>
                                     
                                     <ul class="dropdown-menu dropdown-menu-end">
-                                    @foreach($page->donateCoin as $iten)
-                                        @foreach($iten as $coin)
-                                        <li><a class="dropdown-item" href="#">{{$coin}}</a></li>                  
+                                        @foreach($page->donateCoin as $iten)
+                                            @foreach($iten as $coin)
+                                                <li><a class="dropdown-item">{{$coin}}</a></li>                  
+                                            @endforeach
                                         @endforeach
-                                    @endforeach
                                     </ul>
                                 </div>
             
-                                <div class="row">
-                                    <div class="col-12">
-                                    <button type="button" class="ud-main-btn">{{ $page->t('Donate one time') }}</button>
+                                <button type="button" class="ud-main-btn" id="nextBtn" onclick="nextPrev(1)">{{ $page->t('Donate one time') }}</button>
+                                
+                            </div>
+                            <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                                <div class="tab-pane fade show active mt-5" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                                    @php 
+                                        $countryValues = $page->getCountry(current_path_locale($page));
+                                    @endphp
+                                    <div class="row">
+                                        @if(!empty($countryValues["value"]))  
+                                        @foreach($countryValues["value"] as $value)
+                                            <div class="col-12 col-sm-6 col-lg-4 mb-2"  >
+                                                <a class="ud-main-btn" style=" padding: 20px ;width:100" id="donateValue" onclick="getValue({{ $value }})">{{$countryValues["symbol"]." ".$value }}</a>
+                                            </div>
+                                        @endforeach
+                                        @endif
                                     </div>
-                                </div>
+                    
+                                    <div class="input-group mb-5 mt-5">
+                                        <input type="text" class="form-control" aria-label="Text input with dropdown button">
+                                        <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">{{ current_path_locale($page) }}</button>
+                                        
+                                        <ul class="dropdown-menu dropdown-menu-end">
+                                        @foreach($page->donateCoin as $iten)
+                                            @foreach($iten as $coin)
+                                            <li><a class="dropdown-item" href="#">{{$coin}}</a></li>                  
+                                            @endforeach
+                                        @endforeach
+                                        </ul>
+                                    </div>
+                
+                                    <button type="button" class="ud-main-btn" id="nextBtn" onclick="nextPrev(1)" >{{ $page->t('Donate one time') }}</button>
+            
+                                </div>                    
+                            </div>
         
-                            </div>                    
                         </div>
-    
-                    </div>
-                </div>
-                <form>
+                    </div>                
                     <div class="col-12 tab">
+                        <div class="row">
+                            <div class="col-2">
+                                <button type="button" class="ud-main-btn" id="prevBtn" onclick="nextPrev(-1)"><i class="lni lni-arrow-left"></i></button>
+                            </div>
+                            <div class="col-10 pt-3">
+                                <h4 class="me-2 card-title mb-5">{{ $page->t('Enter your information') }}</h4>
+                            </div>
+                        </div>
                             
-                        <h4 class="me-2 card-title mb-5">{{ $page->t('Enter your information') }}</h4>
-
                         <div class="row">
                             <div class="col-12">
                                 <div class="mb-3">
@@ -140,7 +138,7 @@
 
                         <div class="row">
                             <div class="col-2">
-                                <button class="ud-main-btn" id="prevBtn" onclick="nextPrev(-1)"><i class="lni lni-arrow-left"></i></button>
+                                <button type="button" class="ud-main-btn" id="prevBtn" onclick="nextPrev(-1)"><i class="lni lni-arrow-left"></i></button>
                             </div>
                             <div class="col-10 pt-3">
                                 <h4 class="me-2 card-title mb-5">{{ $page->t('Enter your address') }}</h4>
