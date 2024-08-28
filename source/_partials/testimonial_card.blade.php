d498046a8019060e6cbd2c43385a3a7823be17c1
Crisciany <crisciany.souza@librecode.coop>
2024-08-28 14:25:36 -0400

    remove the tag $page->t()
    Signed-off-by: Crisciany <crisciany.souza@librecode.coop>

--- source/_partials/testimonial_card.blade.php
+++ source/_partials/testimonial_card.blade.php
@@ -1,8 +1,8 @@
 <section class="section_testimonial">
     <p class="title-testimonial text-center mt-3">{{ $page->t('Testimonials') }}</p>
     <h2 class="text-center mb-5">{{ $page->t('What our customers says') }}</h2>
-    <button class="pre-btn"><img src="{{ $page->baseUrl }}assets/images/arrow.png" alt="{{$page->t('Go back pagination of testimonials')}}"></button>
-    <button class="nxt-btn"><img src="{{ $page->baseUrl }}assets/images/arrow.png" alt="{{$page->t('Show more testimonials of the caroseul')}}"></button>
+    <button class="pre-btn"><img src="{{ $page->baseUrl }}assets/images/arrow.png" alt="Go back pagination of testimonials"></button>
+    <button class="nxt-btn"><img src="{{ $page->baseUrl }}assets/images/arrow.png" alt="Show more testimonials of the caroseul"></button>
     <div class="testimonial-container">
         @foreach($page->testimonials as $itens => $topics)
             <div class="testimonial-card">
