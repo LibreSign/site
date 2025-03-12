8053d37d23d23d3c7852ab1c07c5aea395b9e598
Crisciany <crisciany.souza@librecode.coop>
2025-03-12 16:45:05 -0400

    adjustment about style documentation
    Signed-off-by: Crisciany <crisciany.souza@librecode.coop>
    Signed-off-by: Samuelson Brito <samuelsonma@gmail.com>

--- navigation.php
+++ navigation.php
@@ -1,16 +1,33 @@
 <?php
 
 return [
-    'Installation' => [
-        'root' => '/docs/instalation',
-        'children' => [
-            'Using a Starter Template' => ['root' => '/docs/starter-templates'],
-        ],
-    ],
+  
     'Installation2' => [
-        'root' => '/docs/instalation2',
+        'type' => 'user',
+        'root' => '/docs/user/instalation',
         'children' => [
-            'Using a Starter Template2' => ['root' => '/docs/starter-templates2'],
+            'Using a Starter Template' => ['root' => '/docs/user/starter-templates'],
         ],
     ],
+    'Installation1' => [
+        'type' => 'admin',
+        'root' => '/docs/admin/instalation',
+        'children' => [
+            'Using a Starter Template' => ['root' => '/docs/admin/starter-templates'],
+        ],
+    ],
+    'Installation' => [
+        'type' => 'developer',
+        'root' => '/docs/developer/instalation',
+        'children' => [
+            'Using a Starter Template' => ['root' => '/docs/developer/starter-templates'],
+            'Using a Starter Template2' => ['root' => '/docs/developer/starter-templates'],
+            'Using a Starter Template3' => ['root' => '/docs/developer/starter-templates'],
+            'Using a Starter Template4' => ['root' => '/docs/developer/starter-templates'],
+            'Using a Starter Template5' => ['root' => '/docs/developer/starter-templates'],
+            'Using a Starter Template5' => ['root' => '/docs/developer/starter-templates'],
+            'Using a Starter Template6' => ['root' => '/docs/developer/starter-templates'],
+            'Using a Starter Template' => ['root' => '/docs/developer/starter-templates'],
+        ],
+    ],  
 ];
