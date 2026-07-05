31325028f536602dcc1ca7f2db5df803508a2a46
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-07-05 17:50:10 -0300

    feat: update footer.blade.php
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ source/fragments/fr/footer.blade.php
@@ -0,0 +1,7 @@
+---
+title: "LibreSign Footer Fragment"
+description: "Embeddable LibreSign footer fragment for WordPress integration."
+noindex: true
+isFooterFragment: true
+---
+@include('_layouts.footer')
