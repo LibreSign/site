564733467128ae452918cb6a29ab97bbcc45c1e4
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-07-06 12:50:35 -0300

    feat: add header fragment template
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ source/fragments/header.blade.php
@@ -0,0 +1,7 @@
+---
+title: "LibreSign Header Fragment"
+description: "Embeddable LibreSign header fragment for WordPress integration."
+noindex: true
+isHeaderFragment: true
+---
+@include('_layouts.header')
