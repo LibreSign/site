f5cc4cb5a92779e5ebbd17c5cb0de0f75c471fe1
Vitor Mattos <vitor@php.rio>
2026-09-25 16:10:25 -0300

    refactor: initialize tested pronunciation module
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,6 +1,8 @@
-import './brand-pronunciation.js';
+import { initBrandPronunciation } from './brand-pronunciation.mjs';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'animate.css';
+
+initBrandPronunciation();
 
 import AOS from 'aos';
 const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
